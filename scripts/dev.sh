a21ee0c5f3dcda70ebfb0e5780baa4bd9474f984
YogeshGChowdary <yogesh.g.chowdary@gmail.com>
2025-10-17 12:43:03 +0530

    command typo

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -13,7 +13,7 @@ az group create --name $RESOURCE_GROUP_NAME --location canadacentral
 az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STAGE_SA_ACCOUNT --sku Standard_LRS --encryption-services blob
 
 # Create storage account for dev environment
-az storage account create --resource-group4 $RESOURCE_GROUP_NAME --name $DEV_SA_ACCOUNT --sku Standard_LRS --encryption-services blob
+az storage account create --resource-group $RESOURCE_GROUP_NAME --name $DEV_SA_ACCOUNT --sku Standard_LRS --encryption-services blob
 
 # Create blob container for staging environment
 az storage container create --name $CONTAINER_NAME --account-name $STAGE_SA_ACCOUNT
